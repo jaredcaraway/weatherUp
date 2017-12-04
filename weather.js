b8dc3e2b200da508ffce47fb96ecb231392c38db
Jared Caraway <jared.caraway@gmail.com>
2017-12-04 00:37:20 -0600

    Connect interface to API & begin styling

--- weather.js
+++ weather.js
@@ -9,34 +9,34 @@ $(document).ready(function() {
 		longitude: null
 	}
 
-	var weatherIcon = document.querySelector("#weatherIcon div");
+	var weatherIcon = document.querySelector("#weatherIcon div img");
 	var location = document.querySelector("#location div");
 	var weatherInfo = document.querySelector("#weatherInfo div");
 	var temperature = document.querySelector("#temperature div");
 	var convert = document.querySelector("#convert div");
-	var description = document.querySelector("#description div");
+	var description = document.querySelector("#description");
 
 	function toCelsius(temp) {
 		if (temp.unit === "kelvin") {
 			temp.unit = "celsius";
-			return (temp.number - 273.15).toPrecision(2);
+			temp.number = (temp.number - 273.15).toPrecision(2);
 		} else if (temp.unit === "fahrenheit") {
 			temp.unit = "celsius";
-			return ((temp.number - 32) * (5/9)).toPrecision(2);
+			temp.number = ((temp.number - 32) * (5/9)).toPrecision(2);
 		} else {
-			return "NO UNIT SPECIFIED!";
+			console.log("NO UNIT SPECIFIED!");
 		}
 	}
 
 	function toFahrenheit(temp) {
 		if (temp.unit === "kelvin") {
 			temp.unit = "fahrenheit";
-			return ((temp.number - 273.15) * (9/5) + 32).toPrecision(2);
+			temp.number = ((temp.number - 273.15) * (9/5) + 32).toPrecision(2);
 		} else if (temp.unit === "celsius") {
 			temp.unit = "fahrenheit";
-			return ((temp.number * (9/5)) + 32).toPrecision(2);
+			temp.number = ((temp.number * (9/5)) + 32).toPrecision(2);
 		} else {
-			return "NO UNIT SPECIFIED!";
+			console.log("NO UNIT SPECIFIED!");
 		}
 	}
 
@@ -46,14 +46,33 @@ $(document).ready(function() {
 		});
 	}
 
+	var populateInfo = function(data) {
+		location.innerHTML = data.name;
+		temp.number = data.main.temp;
+		weatherIcon.setAttribute("src", data.weather[0].icon);
+		temp.number = data.main.temp;
+		temp.unit = "celsius";
+		toFahrenheit(temp);
+		temperature.innerHTML = temp.number +
+			(temp.unit === "celsius" ? "&deg; C" : "&deg; F");
+		description.innerHTML = data.weather[0].description;
+	}
+
 	function getWeather(position) {
-			$.ajax({
-			url: "https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139",
+		myLocation.latitude = position.coords.latitude.toPrecision(4);
+		myLocation.longitude = position.coords.longitude.toPrecision(4);
+
+		var query = "https://fcc-weather-api.glitch.me/api/current?lat=" + myLocation.latitude + "&lon=" + myLocation.longitude;
+		query += "&units=metric";
+
+		$.ajax({
+			url: query,
 			error: function() {
 				alert("Request failed!");
 			},
 			success: function(data) {
-				
+				populateInfo(data);
+				console.log(data);
 			}
 		});
 	}
@@ -69,3 +88,8 @@ $(document).ready(function() {
 		alert("Geolocation disabled!");
 	}
 });
+
+// Get location
+// Round coords & build query string
+// Get JSON weather data via query
+// Populate page with data info
